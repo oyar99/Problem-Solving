c5ff4e87cf4398f56c4e62426d2e771bd0e9167c
Jhon Rayo <RayoJhon99@gmail.com>
2020-05-03 11:21:35 -0500

    DP

--- LeetCode/LeetCode-DominoAndTrominoTiling.cpp
+++ LeetCode/LeetCode-DominoAndTrominoTiling.cpp
@@ -10,13 +10,13 @@ public:
         dp[1][0] = 1;
         
         for (int i = 2; i <= N; ++i) {
-            dp[i][0] = dp[i-1][3]%MOD;
+            dp[i][0] = dp[i-1][3];
             dp[i][1] = (dp[i-1][0]%MOD + dp[i-1][2]%MOD)%MOD;
             dp[i][2] = (dp[i-1][0]%MOD + dp[i-1][1]%MOD)%MOD;
             
             for (int j = 0; j < 4; ++j) dp[i][3] = (dp[i][3]%MOD + dp[i-1][j]%MOD) % MOD;
         }
         
-        return dp[N][3]%MOD;
+        return dp[N][3];
     }
 };
