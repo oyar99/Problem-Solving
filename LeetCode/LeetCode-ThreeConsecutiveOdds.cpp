444d91b608d94ede5ee33d5ca0a43c129f328b39
Jhon Rayo <RayoJhon99@gmail.com>
2020-09-12 18:46:42 -0500

    Easy

---
+++ LeetCode/LeetCode-ThreeConsecutiveOdds.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for (int i = 0; i < ((int)arr.size()) - 2; ++i) {
+            if (arr[i] & 1 && arr[i+1] & 1 && arr[i+2] & 1) return true;
+        }
+        
+        return false;
+    }
+};
