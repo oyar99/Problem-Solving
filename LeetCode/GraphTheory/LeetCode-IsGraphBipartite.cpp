d4ca0f06e4dbe96792e5991d8fc12ac289a4d4db
Jhon Rayo <RayoJhon99@gmail.com>
2021-02-14 16:35:22 -0400

    Update LeetCode-IsGraphBipartite.cpp

--- LeetCode/GraphTheory/LeetCode-IsGraphBipartite.cpp
+++ LeetCode/GraphTheory/LeetCode-IsGraphBipartite.cpp
@@ -1,82 +1,29 @@
-struct graph {
-    unordered_map<int, unordered_set<int>> adj;
-    
-    void add_edge(int u, int v) {
-        adj[u].insert(v);
-        adj[v].insert(u);
-    }
-};
-
-bool is_bipartite(graph& g, int n) {
-    queue<int> q;
-    
-    unordered_set<int> red;
-    unordered_set<int> blue;
-    unordered_set<int> visited;
-    
-    for (int w = 0; w < n; ++w) {
-        
-        if (visited.count(w) > 0) continue;
-    
-        red.insert(w);
-        q.push(w);
-        visited.insert(w);
-
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            if (red.count(u) > 0) {
-
-                for (int v: g.adj[u]) {
-                    if (visited.count(v) <= 0) {
-                        blue.insert(v);
-                        visited.insert(v);
-                        q.push(v);
-                    }
-                }
-
-            } else {
-
-                for (int v: g.adj[u]) {
-                    if (visited.count(v) <= 0) {
-                        red.insert(v);
-                        visited.insert(v);
-                        q.push(v);
-                    }
-                }
-            }  
-        }
-        
-    }
-    
-    for (int u = 0; u < n; ++u) {
-        if (red.count(u) > 0) {
-            for (int v: g.adj[u]) {
-                if (red.count(v) > 0) return false;
-            }
-        } else {
-            for (int v: g.adj[u]) {
-                if (blue.count(v) > 0) return false;
-            }
-        }
-    }
-    return true;
-    
-}
-
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& l) {
+    bool isBipartite(vector<vector<int>>& graph) {
         
-        graph g;
+        unordered_map<int, int> color;
         
-        for (int i = 0; i < l.size(); ++i) {
-            for (int j = 0; j < l[i].size(); ++j) {
-                g.add_edge(i, l[i][j]);
+        stack<int> s;
+        unordered_set<int> visited;
+
+        for (int i = 0; i < graph.size(); ++i) {
+
+            if (visited.count(i)) continue;
+
+            s.push(i);
+            color[i] = 0;
+
+            while (!s.empty()) {
+                int u = s.top();
+                visited.insert(u);
+                s.pop();
+                for (int v : graph[u])
+                    if (visited.count(v) <= 0) color[v] = !color[u], s.push(v);
+                    else if (color[v] == color[u]) return false;
             }
         }
         
-        return is_bipartite(g, l.size());
+        return true;
     }
 };
