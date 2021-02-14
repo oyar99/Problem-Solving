57e6135dbffbe1cd62847b779574f4805998ea6d
Jhon Rayo <RayoJhon99@gmail.com>
2021-02-14 16:45:43 -0400

    Create LeetCode-DesignParkingSystem.cpp

---
+++ LeetCode/AdHoc/LeetCode-DesignParkingSystem.cpp
@@ -0,0 +1,11 @@
+class ParkingSystem {
+    int b;
+    int m;
+    int s;
+public:
+    ParkingSystem(int big, int medium, int small) : b(big), m(medium), s(small) {}
+    
+    bool addCar(int carType) {
+        return carType == 1 ? --b >= 0 : carType == 2 ? --m >= 0 : --s >= 0;
+    }
+};
