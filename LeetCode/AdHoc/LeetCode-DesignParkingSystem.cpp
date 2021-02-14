ea89214740091e47b4b67fa5ebc473c52530c7e5
Jhon Rayo <RayoJhon99@gmail.com>
2021-02-14 16:59:25 -0400

    Update LeetCode-DesignParkingSystem.cpp

--- LeetCode/AdHoc/LeetCode-DesignParkingSystem.cpp
+++ LeetCode/AdHoc/LeetCode-DesignParkingSystem.cpp
@@ -1,9 +1,1 @@
-class ParkingSystem {
-    int b, m, s;
-public:
-    ParkingSystem(int big, int medium, int small) : b(big), m(medium), s(small) {}
-    
-    bool addCar(int carType) {
-        return carType == 1 ? --b >= 0 : carType == 2 ? --m >= 0 : --s >= 0;
-    }
-};
+struct ParkingSystem{int b,m,s;ParkingSystem(int b,int m,int s):b(b),m(m),s(s){}bool addCar(int c){return !--c?b-->0:!--c?m-->0:s-->0;}};
