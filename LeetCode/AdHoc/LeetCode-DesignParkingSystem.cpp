666b2b3f0b6fb0c4d2ad6ecb63ab56e168df9191
Jhon Rayo <RayoJhon99@gmail.com>
2021-02-14 17:11:27 -0400

    Update LeetCode-DesignParkingSystem.cpp

--- LeetCode/AdHoc/LeetCode-DesignParkingSystem.cpp
+++ LeetCode/AdHoc/LeetCode-DesignParkingSystem.cpp
@@ -1,1 +1,1 @@
-struct ParkingSystem{int b,m,s;ParkingSystem(int b,int m,int s):b(b),m(m),s(s){}bool addCar(int c){return !--c?b-->0:!--c?m-->0:s-->0;}};
+struct ParkingSystem{int b,m,s;ParkingSystem(int B,int M,int S){b=B;m=M;s=S;}bool addCar(int c){return !--c?b-->0:!--c?m-->0:s-->0;}};
