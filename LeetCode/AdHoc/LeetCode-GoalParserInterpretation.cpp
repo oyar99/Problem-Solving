86b4524d713b85b04090b3cc50711d29c928711d
Jhon Rayo <RayoJhon99@gmail.com>
2021-02-20 13:16:46 -0400

    Create LeetCode-GoalParserInterpretation.cpp

---
+++ LeetCode/AdHoc/LeetCode-GoalParserInterpretation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string interpret(const string& command) {
+        string ans = "";
+        
+        for (int i = 0; i < command.size();) {
+            if (command[i] == 'G') ans += "G", ++i;
+            else if (command[i+1] == ')') ans += "o", ++++i;
+            else ans += "al", ++++++++i;
+        }
+        
+        return ans;
+    }
+};
