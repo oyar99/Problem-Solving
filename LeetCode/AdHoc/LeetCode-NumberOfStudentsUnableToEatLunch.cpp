8cbb4c6a8222b451273ae996344a2e8129e0e852
Jhon Rayo <RayoJhon99@gmail.com>
2021-02-20 14:33:48 -0400

    Update LeetCode-NumberOfStudentsUnableToEatLunch.cpp

--- LeetCode/AdHoc/LeetCode-NumberOfStudentsUnableToEatLunch.cpp
+++ LeetCode/AdHoc/LeetCode-NumberOfStudentsUnableToEatLunch.cpp
@@ -1,3 +1,3 @@
 #define v vector<int>&
 #define r return
-struct Solution{int countStudents(v S,v W){int a=0,b=0,n=W.size();for(int s:S)!s?++a:++b;int i=0;for(int w:W){if(!w&&a--);else if(!w)r n-i;if(w&&b--);else if(w)r n-i;++i;}r 0;}};
+struct Solution{int countStudents(v S,v W){int a=0,b=0,n=W.size();for(int s:S)s?++a:++b;int i=0;for(int w:W){if(w&&a--);else if(w)r n-i;if(!w&&b--);else if(!w)r n-i;++i;}r 0;}};
