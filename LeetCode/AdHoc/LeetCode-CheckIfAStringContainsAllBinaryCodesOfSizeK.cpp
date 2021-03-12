6b803d5caff933ff0896b33ef503699479593b2e
Jhon Rayo <RayoJhon99@gmail.com>
2021-03-12 11:00:11 -0400

    bit manipulation

---
+++ LeetCode/AdHoc/LeetCode-CheckIfAStringContainsAllBinaryCodesOfSizeK.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool hasAllCodes(const string& s, int k) {
+        
+        int n = s.size();
+        
+        bool seen [1 << k];
+        memset(seen, 0, sizeof(seen));
+        
+        for (int i = 0; i < n - k + 1; ++i) {
+            
+            int cur = 0;
+            for (int j = 0; j < k; ++j)
+                cur += (s[j+i] == '1' ? 1 << (k - j - 1) : 0);
+            
+            seen[cur] = 1;
+        }
+        
+        for (int i = 0; i < (1 << k); ++i)
+            if (!seen[i]) return false;
+        
+        return true;
+    }
+};
