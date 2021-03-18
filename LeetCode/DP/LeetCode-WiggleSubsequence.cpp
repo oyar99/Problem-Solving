49801675087dc3357054f97fd0cfc776a8351e74
Jhon Rayo <RayoJhon99@gmail.com>
2021-03-18 19:14:21 -0300

    Update LeetCode-WiggleSubsequence.cpp

--- LeetCode/DP/LeetCode-WiggleSubsequence.cpp
+++ LeetCode/DP/LeetCode-WiggleSubsequence.cpp
@@ -7,8 +7,8 @@ public:
         // dp(i, 0) is the length of the longest wiggle subsequence starting at index i, and the first difference is negative
         // dp(i, 1) is the length of the longest wiggle subsequence starting at index i, and the first difference is positve
         
-        // dp(i, 0) = for all j such that nums[j] - nums[i] < 0 && j > i; max(dp(i + j, 1)) + 1
-        // dp(i, 1) = for all j such that nums[j] - nums[i] > 0 && j > i; max(dp(i + j, 0)) + 1
+        // dp(i, 0) = max(dp(i + j, 1)) + 1 for all j such that nums[j] - nums[i] < 0 && j > i
+        // dp(i, 1) = max(dp(i + j, 0)) + 1 for all j such that nums[j] - nums[i] > 0 && j > i
         
         // final answer will be the maximum value for all i in dp(i, 0) and dp(i, 1);
         int dp[n][2];
