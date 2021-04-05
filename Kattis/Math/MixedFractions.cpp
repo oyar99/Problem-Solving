81e740eae950a9d51ca3e97bc3f0811c24a5cdd2
Jhon Rayo <RayoJhon99@gmail.com>
2021-04-04 23:10:50 -0300

    Math

---
+++ Kattis/Math/MixedFractions.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+    while (true) {
+        int a, b; cin >> a >> b;
+
+        if (a == 0 && b == 0) break;
+
+        cout << a / b << " " << a % b << " / " << b << endl; 
+        
+    }
+}
