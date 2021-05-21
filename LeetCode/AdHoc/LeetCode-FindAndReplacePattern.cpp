41455fc0ff284efcbad0bbbf05b4be86949b4229
Jhon Rayo <RayoJhon99@gmail.com>
2021-05-21 11:11:10 -0500

    Update LeetCode-FindAndReplacePattern.cpp

--- LeetCode/AdHoc/LeetCode-FindAndReplacePattern.cpp
+++ LeetCode/AdHoc/LeetCode-FindAndReplacePattern.cpp
@@ -2,30 +2,24 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, const string& pattern) {
         vector<string> ans;
+        ans.reserve(words.size());
         
         for (const string& w: words) {            
             unordered_map<char, char> m;
             
             bool pos = true;
             
-            for (int i = 0; i < w.size(); ++i) {
-                if (m.count(w[i])) {
-                    if (m[w[i]] != pattern[i]) {
-                        pos = false;
-                        break;
-                    }
-                } else {
-                    m[w[i]] = pattern[i];
-                }
-            }
+            for (int i = 0; i < w.size() && pos; ++i)
+                pos = !(m.count(w[i]) && m[w[i]] != pattern[i]),
+                m[w[i]] = pattern[i];
+            
+            if (!pos) continue;
             
             unordered_set<char> s;
             
             for (const auto& e: m) {
-                if (s.count(e.second)) {
-                    pos = false;
-                    break;
-                }
+                pos = !s.count(e.second);
+                if (!pos) break;
                 s.insert(e.second);
             }
             
