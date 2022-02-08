b31948c07d3aff94a6f29ac95f47c65be1d41110
Jhon Rayo <RayoJhon99@gmail.com>
2022-02-07 19:58:32 -0500

    Update LeetCode-MaximumNumberOfWordsYouCanType.cpp

--- LeetCode/AdHoc/LeetCode-MaximumNumberOfWordsYouCanType.cpp
+++ LeetCode/AdHoc/LeetCode-MaximumNumberOfWordsYouCanType.cpp
@@ -1,1 +1,1 @@
-struct Solution{int canBeTypedWords(string&t,string&b){int a=0;vector<bool>d(26);bool p=1;for (char&i:b)d[i-'a']=1;for (char&c:t+' ')a+=p&&c==' ',p=(p||c==' ')&&(c==' '||!d[c-'a']);return a;}};
+struct Solution{int canBeTypedWords(string&t,string&b){int a=0;vector<bool>d(26);bool p=1;for(char&i:b)d[i-'a']=1;for(char&c:t+' ')a+=p&&c==' ',p=(p||c==' ')&&(c==' '||!d[c-'a']);return a;}};
