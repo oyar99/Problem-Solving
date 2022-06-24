31b102030dc6e634f6cb4a69a64e46ffa1f7e688
Jhon Rayo <RayoJhon99@gmail.com>
2022-06-24 13:02:15 -0500

    Create LeetCode-FindAllDuplicatesInAnArray.cpp

---
+++ LeetCode/AdHoc/LeetCode-FindAllDuplicatesInAnArray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (nums[abs(nums[i]) - 1] < 0) {
+                ans.push_back(abs(nums[i]));
+            } else {
+                nums[abs(nums[i]) - 1] *= -1;
+            }
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            nums[i] = abs(nums[i]);
+        }
+        
+        return ans;
+    }
+};
