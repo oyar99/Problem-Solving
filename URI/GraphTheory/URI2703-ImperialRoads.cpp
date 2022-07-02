acc88f62c8220c8db961bffde7835c5ed7acc2b4
Jhon Rayo <jhon.rayo@blackboard.com>
2022-07-01 22:50:35 -0500

    ImperialRoads

--- URI/GraphTheory/URI2703-ImperialRoads.cpp
+++ URI/GraphTheory/URI2703-ImperialRoads.cpp
@@ -4,244 +4,205 @@
 
 using namespace std;
 
-class graph {
-    public:
-        unordered_map<int, unordered_map<int, int>> list;
+const int MAX_LOG = 2e5+1;
+int logs[MAX_LOG];
 
-        unordered_map<int, int> get_neighbours(int u) {
-            return list[u];
-        }
-};
+int mst(vector<unordered_map<int, int>>& adj, vector<pair<pair<int, int>, int>>& edges, vector<unordered_map<int, int>>& adj_t) {
+  int w = 0;
 
-class Tree {
-public:
-    int id;
-    int depth;
-    pair<Tree*, int> parent;
-    vector<pair<Tree*, int>> children;
-};
+  sort(edges.begin(), edges.end(), [](const pair<pair<int, int>, int>& uv, const pair<pair<int, int>, int>& vw) {
+    return uv.second < vw.second;
+  });
 
-class Compare {
-    public:
-    bool operator() (const pair<int, pair<int, int>>& a, const pair<int, pair<int, int>>& b) {
-        return a.second.second > b.second.second;
+  vector<int> disjoint_set(adj.size());
+  vector<int> rank(adj.size());
+
+  for (int i = 1; i < adj.size(); ++i) {
+    disjoint_set[i] = i;
+  }
+
+  function<int(int)> find_set = [&](int u) {
+    if (disjoint_set[u] == u) {
+      return u;
     }
-};
 
+    disjoint_set[u] = find_set(disjoint_set[u]);
+    return disjoint_set[u];
+  };
 
-pair<graph, int> minimum_spanning_tree(graph& g, int u) {
-    graph tree;
-    int weight = 0;
+  function<bool(int, int)> union_set = [&](int u, int v) {
+    int uu = find_set(u);
+    int vv = find_set(v);
 
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, Compare> q;
-    while (g.list.size() != tree.list.size()) {
-        for (auto& v: g.get_neighbours(u)) {
-            if (tree.list.count(v.first) > 0) continue;
-            q.push({u, {v.first, v.second}});
-        }
-
-        auto w = q.top();
-        q.pop();
-        
-        if (tree.list.count(w.first) > 0 && tree.list.count(w.second.first) > 0) continue;
-        
-        tree.list[w.first][w.second.first] = w.second.second;
-        tree.list[w.second.first][w.first] = w.second.second;
-        weight += w.second.second;
-        u = w.second.first;
+    if (uu == vv) {
+      return false;
     }
-    return {tree, weight};
+
+    if (rank[uu] < rank[vv]) {
+      disjoint_set[uu] = vv;
+    } else if (rank[uu] > rank[vv]) {
+      disjoint_set[vv] = uu;
+    } else {
+      disjoint_set[vv] = uu;
+      ++rank[uu];
+    }
+
+    return true;
+  };
+
+  for (int i = 0; i < edges.size(); ++i) {
+    int u = edges[i].first.first;
+    int v = edges[i].first.second;
+
+    if (!union_set(u, v)) continue;
+
+    adj_t[u][v] = adj_t[v][u] = adj[u][v];
+    w += adj[u][v];
+  }
+
+  return w;
 }
 
+void et(vector<unordered_map<int, int>>& adj, vector<int>& tour_out, vector<int>& h_out, vector<vector<int>>& p, int sz) {
+  vector<int> s;
+  s.reserve(adj.size());
+  vector<bool> visited(adj.size(), false);
+  vector<int> h(adj.size());
 
-Tree* graph_to_tree(graph& g, int root, vector<Tree*>& nodes) {
-    Tree* tree = new Tree();
-    queue<pair<Tree*,int>> q;
-    unordered_set<int> visited;
-    q.push({tree,root});
-    visited.insert(root);
-    while (!q.empty()) {
-        int w = q.front().second;
-        Tree* t = q.front().first;
-        t->id = w;
-        nodes[w] = t;
-        q.pop();
-        for (auto& n: g.get_neighbours(w)) {
-            if (visited.count(n.first) <= 0) {
-                visited.insert(n.first);
-                Tree* sub = new Tree();
-                sub->id = n.first;
-                if (sub->id == root) {
-                    tree->depth = 0;
-                    sub->parent.first = nullptr;
-                }
-                else { 
-                    sub->parent.first = t;
-                    sub->parent.second = n.second;
-                    sub->depth = t->depth + 1;
-                }
-                t->children.push_back({sub, n.second});
-                q.push({sub, n.first});
-            }
-        }
+  s.push_back(1);
+  h[1] = 1;
+
+  while (!s.empty()) {
+    int u = s.back();
+    s.pop_back();
+
+    tour_out.push_back(u);
+    h_out.push_back(h[u]);
+
+    if (!visited[u]) {
+      visited[u] = true;
+
+      for (int i = 1; i < sz; ++i) {
+        p[i][u] = p[i-1][p[i-1][u]];
+      }
+
+      for (const pair<int, int>& w : adj[u]) {
+        int v = w.first;
+        int c = w.second;
+
+        if (visited[v]) continue;
+
+        s.push_back(u);
+        s.push_back(v);
+
+        h[v] = h[u] + 1;
+        p[0][v] = u;
+      }
     }
-    return tree;
+  }
 }
 
-void build_euler_tour(Tree* t, vector<pair<int, int>>& out_table, vector<int>& leaves) {
-    stack<Tree*> s;
-    unordered_set<int> visited;
-    unordered_map<int, int> inds;
-    int height = 0;
-    s.push(t);
-    visited.insert(t->id);
-    while (!s.empty()) {
-        auto v = s.top();
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int N, R;
+  cin >> N >> R;
 
-        if (inds.count(v->id) <= 0) {
-            out_table.push_back({v->id, height});
-            inds[v->id] = height;
-        }
-        else {
-            out_table.push_back({v->id, inds[v->id]});
-        }
+  vector<unordered_map<int, int>> adj(N + 1);
+  vector<pair<pair<int, int>, int>> edges(R);
 
+  for (int i = 0; i < R; ++i) {
+    int u, v, c;
+    cin >> u >> v >> c;
+    adj[u][v] = adj[v][u] = c;
+    edges[i] = { make_pair(u, v), c };
+  }
 
-        bool all = true;
-        for (auto& child: v->children) {
-            if (visited.count(child.first->id) <= 0) {
-                visited.insert(child.first->id);
-                s.push(child.first);
-                all = false;
-                break;
-            }
-        } 
+  logs[1] = 0;
+  for (int i = 2; i < MAX_LOG; ++i) {
+    logs[i] = logs[i / 2] + 1;
+  }
 
-        if (all) {
-            s.pop();
-            leaves.push_back(v->id);
-        }
-        ++height;
+  vector<unordered_map<int, int>> adj_t(N + 1);
+  int mst_ans = mst(adj, edges, adj_t);
+
+  vector<int> tour;
+  tour.reserve(2*N);
+  vector<int> h;
+  h.reserve(2*N);
+  vector<vector<int>> p(logs[N + 1] + 1, vector<int>(adj.size()));
+  et(adj_t, tour, h, p, logs[N + 1] + 1);
+
+  vector<int> fo(adj_t.size(), -1);
+  for (int i = 0; i < tour.size(); ++i) {
+    if (fo[tour[i]] != -1) continue;
+    fo[tour[i]] = i;
+  }
+
+  int ts = tour.size();
+  auto min_by_h = [&](int i, int j) {
+    return h[i] < h[j] ? i : j;
+  };
+
+  vector<vector<int>> st(logs[ts] + 1, vector<int>(ts));
+
+  for (int i = 0; i < ts; ++i) {
+    st[0][i] = i;
+  }
+
+  for (int i = 1; i < logs[ts] + 1; ++i) {
+    for (int j = 0; j + (1 << i) <= ts; ++j) {
+      st[i][j] = min_by_h(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
     }
-}
+  }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int N, R;
-    cin >> N >> R;
-    graph g;
+  vector<vector<int>> st_act(logs[N + 1] + 1, vector<int>(N + 1));
 
-    for (int i = 0; i < R;++i) {
-        int u, v, c;
-        cin >> u >> v >> c;
-        g.list[u][v] = c;
-        g.list[v][u] = c;
+  for (int i = 1; i <= N; ++i) {
+    st_act[0][i] = adj_t[i][p[0][i]];
+  }
+
+  for (int i = 1; i < logs[N + 1] + 1; ++i) {
+    for (int j = 1; j < N + 1; ++j) {
+      st_act[i][j] = max(st_act[i - 1][j], st_act[i - 1][p[i - 1][j]]);
     }
+  }
 
-    auto tree = minimum_spanning_tree(g, 1);
-    vector<Tree*> nodes(tree.first.list.size() + 1, nullptr);
-    Tree* t = graph_to_tree(tree.first, 1, nodes);
-    vector<pair<int, int>> euler_table;
-    vector<int> first_ocurrence(tree.first.list.size() + 1, -1);
-    vector<int> leaves;
-    build_euler_tour(t, euler_table, leaves);
-    vector<vector<int>> sparse_table(ceil(log2(euler_table.size()) + 1), vector<int>(euler_table.size() + 1));
-    vector<vector<int>> sparse_table_dists(ceil(log2(tree.first.list.size()) + 1), vector<int>(tree.first.list.size() + 1));
-    vector<vector<int>> sparse_table_ances(ceil(log2(tree.first.list.size()) + 1), vector<int>(tree.first.list.size() + 1));
-    unordered_map<int, int> map;
-    vector<int> logs(N+1);
+  int Q;
+  cin >> Q;
+  for (int i = 0; i < Q; ++i) {
+    int u, v;
+    cin >> u >> v;
 
-    logs[1] = 0;
-    for (int i = 2; i <= N; ++i) {
-        logs[i] = logs[i / 2] + 1;
-    }
-    
-    for (int i = 0; i < euler_table.size();++i) {
-        sparse_table[0][i] = euler_table[i].second;
-        map[euler_table[i].second] = euler_table[i].first;
-
-        if (first_ocurrence[euler_table[i].first] != -1) continue;
-        first_ocurrence[euler_table[i].first] = i;
-    }
-    
-    for (int i = 1; i < sparse_table.size(); ++i) {
-        for (int j = 0; j + (1 << i) <= sparse_table[i].size();++j) {
-            sparse_table[i][j] = min(sparse_table[i - 1][j], sparse_table[i - 1][j + (1 << (i - 1))]);
-        }
+    if (adj_t[u][v] > 0) {
+      cout << mst_ans << endl;
+      continue;
     }
     
-    auto t1 = chrono::high_resolution_clock::now();
-    unordered_set<int> visited;
-    for (int u = 0; u < leaves.size(); ++u) {
-        int w = leaves[u];
-        vector<int> ancestor;
-        vector<int> path;
-        vector<int> nodes_vis;
-        while (nodes[w]->parent.first != nullptr) {
-            path.push_back(nodes[w]->parent.second);
-            ancestor.push_back(nodes[w]->parent.first->id);
-            nodes_vis.push_back(w);
-            visited.insert(w);
-            w = nodes[w]->parent.first->id;
-        }
-        for (int i = 0; i < path.size(); ++i) {
-            sparse_table_dists[0][nodes_vis[i]] = path[i];
-            sparse_table_ances[0][nodes_vis[i]] = ancestor[i];
-        }
+    int iu = fo[u];
+    int iv = fo[v];
 
-        for (int i = 1; i < ceil(log2(path.size())) + 1;++i) {
-            for (int j = 0; j + (1 << i) <= path.size(); ++j) {
-                sparse_table_dists[i][nodes_vis[j]] = max(sparse_table_dists[i-1][nodes_vis[j]], sparse_table_dists[i-1][nodes_vis[j + (1 << (i-1))]]);
-                sparse_table_ances[i][nodes_vis[j]] = sparse_table_ances[i-1][nodes_vis[j + (1 << (i-1))]];
-            }
-        }
+    if (iu > iv) {
+      swap(iu, iv);
     }
-    auto t2 = chrono::high_resolution_clock::now();
-    int duration = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
-    cout << "PREPROCESSING: " << duration << " milliseconds" << endl;
 
-    int Q;
-    cin >> Q;
-    for (int i = 0; i < Q;++i) {
-        int u, v;
-        cin >> u >> v;
+    int lca = tour[min_by_h(st[logs[iv - iu]][iu], st[logs[iv - iu]][iv - (1 << logs[iv - iu])])];
 
-        if (tree.first.list[u].count(v) > 0) {
-            cout << tree.second << endl;
-        }
-        else {
+    int max_e = 0;
+    int uu = u;
+    int vv = v;
 
-            int index_u = first_ocurrence[u];
-            int index_v = first_ocurrence[v];
-
-            if (index_u > index_v) {
-                swap(index_u, index_v);
-            }
-
-            index_v += 1;
-            int log_ = floor(log2(index_v - index_u));
-            int suffix = 1 << log_;
-            int lca = min(sparse_table[log_][index_u], sparse_table[log_][index_v - suffix]);
-
-            
-            int max_u = 0;
-            int max_v = 0;
-            int s = u;
-            int r = v;
-
-            while (nodes[s]->depth != nodes[map[lca]]->depth) {
-                max_u = max(max_u, sparse_table_dists[logs[(nodes[s]->depth - nodes[map[lca]]->depth)]][s]);  
-                s = sparse_table_ances[logs[(nodes[s]->depth - nodes[map[lca]]->depth)]][s];
-            }
-            
-            while (nodes[r]->depth != nodes[map[lca]]->depth) {
-                max_v = max(max_v, sparse_table_dists[logs[(nodes[r]->depth - nodes[map[lca]]->depth)]][r]);
-                r = sparse_table_ances[logs[(nodes[r]->depth - nodes[map[lca]]->depth)]][r];     
-            }
-            
-            int max_cost = max(max_u, max_v);
-            cout << tree.second - max_cost + g.list[u][v] << endl;
-        }
+    while (h[fo[uu]] != h[fo[lca]]) {
+      max_e = max(max_e, st_act[logs[h[fo[uu]] - h[fo[lca]]]][uu]);
+      uu = p[logs[h[fo[uu]] - h[fo[lca]]]][uu];
     }
+
+    while (h[fo[vv]] != h[fo[lca]]) {
+      max_e = max(max_e, st_act[logs[h[fo[vv]] - h[fo[lca]]]][vv]);
+      vv = p[logs[h[fo[vv]] - h[fo[lca]]]][vv];
+    }
+
+    cout << mst_ans - max_e + adj[u][v] << endl;
+  }
 }
